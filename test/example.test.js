a201a031c542aa4934beeecf580fdf3543a45dc6
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-09-30 13:52:47 -0700

    Failing test for add function

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,16 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { add } from '../calculations.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('add function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 5;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = add(2, 3);
 
     //Expect
     // Make assertions about what is expected versus the actual result
