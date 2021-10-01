056eab9ba821ea1bdaa1564386086d1be5031332
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-10-01 12:43:30 -0700

    added subtract UI and button listener

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const addInput2 = document.getElementById('add-input-2');
 const addResult = document.getElementById('add-result');
 const addButton = document.getElementById('add-button');
 
+const subInput1 = document.getElementById('sub-input-1');
+const subInput2 = document.getElementById('sub-input-2');
+const subResult = document.getElementById('sub-result');
+const subButton = document.getElementById('sub-button');
+
 addButton.addEventListener('click', ()=> {
     const x = Number(addInput1.value); 
     const y = Number(addInput2.value); 
@@ -12,3 +17,8 @@ addButton.addEventListener('click', ()=> {
     const result = add(x, y);
     addResult.textContent = result;
 });
+
+subButton.addEventListener('click', ()=> {
+    console.log('Subtract button clicked');
+});
+
